314f2423ce7bdfe0e3088856af66844c8a219bba
barlog <barlog7@yandex.ru>
2024-06-28 16:54:26 +0300

    add step 2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,4 +1,24 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
 public class App {
+
+    public static void main(String[] args) throws Exception {
+        var app = getApp();
+
+        app.start();
+    }
+    public static Javalin getApp() throws Exception {
+/*        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });*/
+
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+        return app;
+    }
 }
