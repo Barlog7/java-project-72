910ccc6889792cb71160abb787f2dcf3dd5f8d8e
barlog <barlog7@yandex.ru>
2024-07-16 12:30:35 +0300

    change ID to id

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -3,17 +3,17 @@ DROP TABLE IF EXISTS urls;
 
 
 CREATE TABLE urls (
-    ID  SERIAL PRIMARY KEY NOT NULL,
+    id  SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE url_checks (
-    ID  SERIAL PRIMARY KEY NOT NULL,
+    id  SERIAL PRIMARY KEY NOT NULL,
     statusCode integer NOT NULL,
     title VARCHAR(255),
     h1 VARCHAR(255),
     description text,
-    urlid bigint REFERENCES urls (ID),
+    urlid bigint REFERENCES urls (id),
     created_at TIMESTAMP NOT NULL
 );
