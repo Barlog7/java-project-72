114d367db7fe0a67002f7f91d17844b8622c70fa
barlog <barlog7@yandex.ru>
2024-07-16 12:04:50 +0300

    fix base columns

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS urls;
 
 
 CREATE TABLE urls (
-    ID  SERIAL PRIMARY KEY,
+    ID  SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE url_checks (
-    ID  SERIAL PRIMARY KEY,
-    statusCode integer,
+    ID  SERIAL PRIMARY KEY NOT NULL,
+    statusCode integer NOT NULL,
     title VARCHAR(255),
     h1 VARCHAR(255),
     description text,
